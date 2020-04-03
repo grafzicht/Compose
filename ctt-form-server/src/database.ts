a0389e4592af03ad73da9e1e70f681c8aed2248e
Sven Mol <s.mol@student.utwente.nl>
2020-04-03 12:14:40 +0200

    Add Auth for ctt-form-server

--- ctt-form-server/src/database.ts
+++ ctt-form-server/src/database.ts
@@ -1,6 +1,10 @@
 import {Collection, Db, MongoClient, ObjectId} from 'mongodb'
 
 const client = new MongoClient('mongodb://grafzicht.nl:27017', {
+	auth:{
+		user:"root",
+		password:"H3Awl7yd"
+	},
 	useUnifiedTopology: true
 });
 let collection: Collection;
@@ -15,4 +19,4 @@ export const updateDocument = (id: string, update: object) => collection.updateO
 export const removeDocument = (id: string) => {
 	collection.deleteOne({_id: id});
 	return collection.deleteOne({_id: new ObjectId(id)});
-}
+};
