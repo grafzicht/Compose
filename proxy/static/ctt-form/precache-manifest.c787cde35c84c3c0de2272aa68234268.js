791757bcacf4ba0c360237e26245f891ac6e49b3
Sven Mol <s.mol@student.utwente.nl>
2020-07-13 20:40:31 +0200

    Extract static archive

---
+++ proxy/static/ctt-form/precache-manifest.c787cde35c84c3c0de2272aa68234268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d81fe72168cb28c405875c42849007",
+    "url": "/ctt-form/index.html"
+  },
+  {
+    "revision": "3cdeffd32dc5dcbc77a3",
+    "url": "/ctt-form/static/css/main.29e25814.chunk.css"
+  },
+  {
+    "revision": "f58c3170f6018eb7a542",
+    "url": "/ctt-form/static/js/2.cc1b50e9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ctt-form/static/js/2.cc1b50e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdeffd32dc5dcbc77a3",
+    "url": "/ctt-form/static/js/main.72a3c5f1.chunk.js"
+  },
+  {
+    "revision": "7c0a59537c0eac2e8d04",
+    "url": "/ctt-form/static/js/runtime-main.ff9dd3fc.js"
+  },
+  {
+    "revision": "cf8c88ca6b8bb3370f715a8efc1d6765",
+    "url": "/ctt-form/static/media/arrow.cf8c88ca.svg"
+  }
+]);
